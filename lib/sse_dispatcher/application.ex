30481ffd5cf2b87d16b2f594aedf882b34ecb785
Bertrand Paquet <bertrand.paquet@gmail.com>
2024-03-04 08:31:26 +0100

    Allow to configure port

--- lib/sse_dispatcher/application.ex
+++ lib/sse_dispatcher/application.ex
@@ -1,16 +1,20 @@
 defmodule SSEDispatcher.Application do
   @moduledoc false
 
+  require Logger
+
   use Application
 
   @impl true
   @spec start(any(), any()) :: {:error, any()} | {:ok, pid()}
   def start(_type, _args) do
+    {sse_port, _} = Integer.parse(System.get_env("SSE_PORT") || "4000")
+    {rest_port, _} = Integer.parse(System.get_env("REST_PORT") || "3000")
+    Logger.info("Starting SSEDispatcher on port #{sse_port} for SSE and #{rest_port} for REST")
     children = [
-      {Phoenix.PubSub,
-       name: SSEDispatcher.PubSub, options: [adapter: Phoenix.PubSub.PG2, pool_size: 10]},
-      {Plug.Cowboy, scheme: :http, plug: Rest, options: [port: 3000]},
-      {Plug.Cowboy, scheme: :http, plug: Sse, options: [port: 4000]}
+      {Phoenix.PubSub, name: SSEDispatcher.PubSub, options: [adapter: Phoenix.PubSub.PG2, pool_size: 10]},
+      {Plug.Cowboy, scheme: :http, plug: Rest, options: [port: rest_port]},
+      {Plug.Cowboy, scheme: :http, plug: Sse, options: [port: sse_port]}
     ]
 
     opts = [strategy: :one_for_one, name: SSEDispatcher.Supervisor]
